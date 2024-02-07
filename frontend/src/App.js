3ef50c139801be479b5daaca77052721385395ef
amotor-AM <as64660@gmail.com>
2024-02-07 13:37:10 -0800

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/">Minted Threads</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
